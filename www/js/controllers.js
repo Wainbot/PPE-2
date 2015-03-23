70476aafecbbf420983ed66e39d83b0bce386cf6
jeremy <jeremy.froment@educlever.com>
2015-03-23 18:15:43 +0100

    premier design final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,5 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-    $scope.history = [
-        { date: '27/11/2014', results: 64 },
-        { date: '29/11/2014', results: 9  },
-        { date: '29/11/2014', results: 1  },
-        { date: '08/12/2014', results: 11 },
-        { date: '27/12/2014', results: 26 },
-        { date: '01/01/2015', results: 42 },
-        { date: '01/01/2015', results: 0 },
-        { date: '06/02/2015', results: 86 }
-    ]
+        $scope.dateBegin = new Date();
 });
